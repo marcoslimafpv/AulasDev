8f80f4cd8b7ff38920056e13ba16deb73d557ce8
Marcos <marcoslima54@outlook.com.br>
2021-04-17 11:57:18 -0300

    Atividade 01

---
+++ Atividade 01.py
@@ -0,0 +1,13 @@
+peso = float (input ("Digite o seu peso: "))
+print("Seu peso é: ", peso)
+
+altura = float (input ("Digite sua altura: "))
+print("Sua altura é: ", altura)
+
+imc = peso/(altura*altura)
+
+print("Seu IMC é: ",imc)
+
+
+
+
